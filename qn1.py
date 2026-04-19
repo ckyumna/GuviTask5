daeed69fa0825575e3ae6b1c26454325b777546c
test <test@gmail.com>
2026-04-19 12:41:37 +0530

    qn.1 updated

--- qn1.py
+++ qn1.py
@@ -1,9 +1,13 @@
 # ----------------------------Qn no.1----------------------------------------------
+
+#list of dicts
 list1 = [{'name':'yumna','age':25}, {'name':'alice', 'age':20}, {'name':'bob', 'age':15},{'name':'John', 'age':10},
          {'name':'Harry', 'age':28}]
 
-
+#lambda function to find age > = 18
 list2 = list(filter(lambda item: item['age'] >=18, list1))
 
+
+#using map() to select only names from the new list
 names= list(map(lambda item: item['name'], list2))
 print(names)
